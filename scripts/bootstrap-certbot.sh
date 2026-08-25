9a67a1cff86a8a86cb5902debf414e092960a93b
ROA <rbn.olender@gmail.com>
2026-08-26 01:31:02 +0200

    ops: support apex and www TLS domains

--- scripts/bootstrap-certbot.sh
+++ scripts/bootstrap-certbot.sh
@@ -7,5 +7,5 @@ cd "$project_dir"
 : "${LETSENCRYPT_EMAIL:?LETSENCRYPT_EMAIL doit être défini}"
 
 docker compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.certbot.yml up -d db app nginx
-docker compose -f docker-compose.yml -f docker-compose.prod.yml --profile certbot run --rm certbot certonly --webroot --webroot-path /var/www/certbot --email "$LETSENCRYPT_EMAIL" --agree-tos --no-eff-email -d "$DOMAIN"
+docker compose -f docker-compose.yml -f docker-compose.prod.yml --profile certbot run --rm certbot certonly --webroot --webroot-path /var/www/certbot --email "$LETSENCRYPT_EMAIL" --agree-tos --no-eff-email -d "$DOMAIN" -d "www.$DOMAIN"
 docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --force-recreate nginx
